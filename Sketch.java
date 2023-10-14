086d981b555fda9b309875f6aba86d1dabcc3493
dankisdead <dong.jin25@ycdsbk12.ca>
2023-10-14 15:52:09 -0400

    hi

--- Sketch.java
+++ Sketch.java
@@ -1,36 +1,77 @@
 import processing.core.PApplet;
+import java.util.Random;
+import processing.core.PFont;
 
 public class Sketch extends PApplet {
-	
-	
-  /**
-   * Called once at the beginning of execution, put your size all in this method
-   */
+
   public void settings() {
 	// put your size call here
-    size(400, 400);
+    size(500, 500);
   }
 
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
   public void setup() {
-    background(210, 255, 173);
+    background(135, 206, 235);
   }
 
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
-  public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+    // random generator
+    Random sunRandom = new Random();
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+    // randomized x and y coordinates for location of sun
+    int randomSunPositionX = sunRandom.nextInt(500);
+    int randomSunPositionY = sunRandom.nextInt(500); 
+
+  public void draw() {  
+
+    if (randomSunPositionY <= 125){
+      background(137,207,240);
+    } 
+
+    else if (randomSunPositionY <= 250 && randomSunPositionY >= 125){
+      background(0,0,255);
+    }
+
+    else if (randomSunPositionY <= 375 && randomSunPositionY >= 250){
+      background (255,255,0);
+    }
+
+    else {
+      background (204,85,0);
+    }
+  
+    // draw sun now
+    fill(255,191,0);
+    ellipse(randomSunPositionX,randomSunPositionY,100,100);
+
+    // stem
+    stroke(53,94,59);
+    strokeWeight(15);
+    line(250,350,250,500);
+    
+    // change strokeweight back to normal
+    strokeWeight(1);
+    stroke(0);
+
+    // petals
+    fill(255,254,224);
+    ellipse(300,300,100,100);
+    ellipse(200,300,100,100);
+    ellipse(300,400,100,100);
+    ellipse(200,400,100,100);
+
+    // middle of flower
+    fill(254,229,86);
+    ellipse(250,350,100,100);
+
+    // font
+    PFont myFont = createFont("Arial Bold", 17);
+    textFont(myFont, 17);
+
+    // display date
+    fill (0,0,0);
+    text(year() + "/" + month() + "/" + day(), 375,400);
+    text(hour() + " : " + minute() + " : " + second(), 375, 450);
+ 
   }
   
-  // define other methods down here.
+  
 }
